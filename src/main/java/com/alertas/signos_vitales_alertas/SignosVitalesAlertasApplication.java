187a9012a90189d1a9e27ea8642c474cf4d4c3f5
Manuel Valdés <manuelvaldesg97@gmail.com>
2025-02-17 04:22:15 -0300

    Generación proyecto backend para almacenar alertas médicas en BD Oracle Cloud, las cuales se  obtienen desde tópico Kafka.

---
+++ src/main/java/com/alertas/signos_vitales_alertas/SignosVitalesAlertasApplication.java
@@ -0,0 +1,13 @@
+package com.alertas.signos_vitales_alertas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignosVitalesAlertasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignosVitalesAlertasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alertas/signos_vitales_alertas/SignosVitalesAlertasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alertas.signos_vitales_alertas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignosVitalesAlertasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
